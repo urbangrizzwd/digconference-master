33d645e822d8c5ff71d1b08ad7b2360e04d76279
urbangrizzwd <urbangrizzwd@gmail.com>
2023-04-18 21:54:06 -0500

    Starting filter logic

--- src/js/App.js
+++ src/js/App.js
@@ -5,3 +5,27 @@ if (import.meta.hot) {
     console.log("HMR")
   });
 }
+
+const buttons = document.querySelectorAll('button');
+const cards = document.querySelectorAll('.workshop-card')
+buttons && buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+
+    const className = button.querySelector('p').innerText;
+    e.preventDefault()
+
+    // Loop through cards
+
+    cards && cards.forEach((card) => {
+      const categories = card.dataset.categories
+      console.log(className);
+      const formated = categories.replace(/[|]/g, '').split(',');
+      console.log(formated.length);
+      if (formated.includes(className)) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
+    })
+  })
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,14 @@ module.exports = {
     safelist: [],
     theme: {
         fontFamily: {
-            titillium: 'Titillium Web, sans-serif', 
+            titillium: 'Titillium Web, sans-serif',
         },
         extend: {
             colors: {
                 primary: '#1E3358',
                 secondary: '#F15A29',
                 tertiary: '#4D2E90',
-                quaternary: '#EC297B',
+                quaternary: '#EC297B'
 
             },
             fontSize: {},
